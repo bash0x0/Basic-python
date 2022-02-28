404c108545830d7af775c0014c1c03dbc671a792
MonkeySkid <100307264+MonkeySkid@users.noreply.github.com>
2022-02-28 15:53:59 +0100

    Create hello world.py

---
+++ hello world.py
@@ -0,0 +1,8 @@
+# This prints "hello world" in your console
+print('hello world')
+
+# This makes your code sleep for 5 seconds
+import time
+time.sleep(5)
+
+### More will be added soon -- bash and monkeyskid
